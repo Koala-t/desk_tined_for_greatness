6bdda458783665cb4a98e4a8dfcf211e2662a5e5
Chris Johnson <johnsonc.88@gmail.com>
2016-09-23 16:52:20 -0700

    refactor database to have a single users table rather than a students table and a teachers table

--- app/models/teachers_students.rb
+++
@@ -1,4 +0,0 @@
-class TeachersStudents < ActiveRecord::Base
-  belongs_to :teacher
-  belongs_to :student
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-TeachersStudents.delete_all
-Teacher.delete_all
-Student.delete_all
-Result.delete_all
+# TeachersStudents.delete_all
+# Teacher.delete_all
+# Student.delete_all
+# Result.delete_all
 User.delete_all
 
-students = ["tommy", "tammy", "terence", "Lilly"]
+# students = ["tommy", "tammy", "terence", "Lilly"]
 
-students.each do |name|
-  Student.create(name: name)
-end
+# students.each do |name|
+#   Student.create(name: name)
+# end
 
-teacher = Teacher.create(name: "Mr/Mrs Smith")
+# teacher = Teacher.create(name: "Mr/Mrs Smith")
 
-results = [
-  {first_number: 1, second_number: 1, correct: true, question_type: "addition", student: Student.find_by(name: "tommy")},
-  {first_number: 2, second_number: 2, correct: true, question_type: "addition", student: Student.find_by(name: "tommy")},
-  {first_number: 3, second_number: 3, correct: true, question_type: "addition", student: Student.find_by(name: "tammy")},
-  {first_number: 4, second_number: 4, correct: true, question_type: "addition", student: Student.find_by(name: "tammy")},
-  {first_number: 5, second_number: 5, correct: true, question_type: "addition", student: Student.find_by(name: "terence")},
-  {first_number: 6, second_number: 6, correct: true, question_type: "addition", student: Student.find_by(name: "terence")},
-  {first_number: 7, second_number: 7, correct: true, question_type: "addition", student: Student.find_by(name: "Lilly")},
-  {first_number: 8, second_number: 8, correct: true, question_type: "addition", student: Student.find_by(name: "Lilly")},
-]
+# results = [
+#   {first_number: 1, second_number: 1, correct: true, question_type: "addition", student: Student.find_by(name: "tommy")},
+#   {first_number: 2, second_number: 2, correct: true, question_type: "addition", student: Student.find_by(name: "tommy")},
+#   {first_number: 3, second_number: 3, correct: true, question_type: "addition", student: Student.find_by(name: "tammy")},
+#   {first_number: 4, second_number: 4, correct: true, question_type: "addition", student: Student.find_by(name: "tammy")},
+#   {first_number: 5, second_number: 5, correct: true, question_type: "addition", student: Student.find_by(name: "terence")},
+#   {first_number: 6, second_number: 6, correct: true, question_type: "addition", student: Student.find_by(name: "terence")},
+#   {first_number: 7, second_number: 7, correct: true, question_type: "addition", student: Student.find_by(name: "Lilly")},
+#   {first_number: 8, second_number: 8, correct: true, question_type: "addition", student: Student.find_by(name: "Lilly")},
+# ]
 
-results.each do |result|
-  Result.create(result)
-end
+# results.each do |result|
+#   Result.create(result)
+# end
 
 
-Student.all.each do |student|
-  TeachersStudents.create(student: student, teacher: teacher)
-end
+# Student.all.each do |student|
+#   TeachersStudents.create(student: student, teacher: teacher)
+# end
 
 users = ["tommy", "tammy", "terence", "Lilly", "Mr Smith"]
 
@@ -40,4 +40,10 @@ users.each do |user|
   else
     User.create(name: user, is_teacher: true)
   end
+end
+
+teacher = User.find_by(is_teacher: true)
+
+User.all.each do |user|
+  teacher.students << user if !user.is_teacher
 end
