5189d91cd1cb8159b498d19fd338dc40b7e86e64
Chris Johnson <johnsonc.88@gmail.com>
2016-09-26 14:35:04 -0700

    add ajax route for login sequence

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,14 +1,34 @@
 get '/sessions/new' do
-  erb :'partials/_loginForm'
+  if request.xhr?
+    erb :'partials/_loginForm', layout: false
+  else
+    erb :'partials/_loginForm'
+  end
 end
 
 post '/sessions' do
   if User.find_by(name: params[:name]) && User.find_by(id: params[:id])
     session[:id] = params[:id]
-    
-    redirect :"/users/#{session[:id]}"
+    @user = currentUser
+    if request.xhr?
+          p '_______________________ajax__________________'
+
+      if currentUser.is_teacher
+        p '____________________teacher_____________'
+        erb :'partials/_teacherMenu', layout: false
+      else
+        # why does this line run twice
+        p '____________________student_____________'
+        erb :'partials/_studentMenu', layout: false
+      end
+    else
+      p '____________________not___ajax______1____________'
+      redirect :"/users/#{session[:id]}"
+    end
+
   else
     # add message for incorrect entry with ajax
+    p '____________________not___ajax________2__________'
     redirect :'/sessions/new'
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  displayLoginForm();
+  login();
 });
+
+function displayLoginForm() {
+  $("#navBar").on("click", "#signIn", function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: "get",
+      url: '/sessions/new'
+    })
+      .done(function(form){
+        $("#navBar").append(form); 
+        $("#signIn").css("display", "none"); 
+      });
+  });
+};
+
+function login() {
+  $("#navBar").on("click", "#loginButton", function(event){
+    event.preventDefault();
+    var form = $(this).parent()[0];
+    var method = $(this).parent()[0].method;
+    var url = $(this).parent()[0].action;
+    var data = $(form).serialize();
+
+    $.ajax({
+      method: method,
+      url: url,
+      data: data
+    })
+      .done(function(menu){
+        console.log(menu);
+        // alert('in done')
+        $("#loginForm").css("display", "none");
+        $("#menu").append(menu);
+        $("#menu").css("display", "block");
+      });
+  });
+};
